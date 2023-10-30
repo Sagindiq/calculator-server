37e9242d6c48e7b5b96d4863f1ab58609663b234
sagindiq <sogindiq77@gmail.com>
2023-10-30 13:50:09 +0500

    configurations changed to deploy

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -3,11 +3,11 @@ import { DataSource } from "typeorm";
 
 export const dataSource = new DataSource({
     type: 'postgres',
-    host: 'localhost',
+    host: 'rosie.db.elephantsql.com',
     port: 5432,
-    username: 'postgres',
-    password: 'XackPro0893',
-    database: 'exam_maket',
+    username: 'vieywpvg',
+    password: '3DlM8TeI-jNpHUH0lWiHB09hEmiuYZYA',
+    database: 'vieywpvg',
     synchronize: true,
     entities: [path.join(__dirname, '..', 'entities', '*.entity.{ts,js}')]
 })
